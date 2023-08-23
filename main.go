c580766a682b2833b676d7b7b3821a5a15b7975b
DevitoDbug <Davidochiengy@gmail.com>
2023-08-23 12:43:44 +0300

    Created the getMovies and deletMovies handlers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -22,6 +23,25 @@ type Director struct {
 // Movies -Storing our movies
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(movies)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+}
+
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
+}
+
 func main() {
 	port := ":8000"
 	r := mux.NewRouter()
@@ -33,6 +53,16 @@ func main() {
 		Director: &Director{
 			FirstName: "David",
 			LastName:  "Ochieng",
+		},
+	})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "9678963",
+		Title: "The second",
+		Director: &Director{
+			FirstName: "Gerald",
+			LastName:  "Bahati",
 		},
 	})
 
