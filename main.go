063e29b7ad1114d3c2228d052c281128b459815f
DevitoDbug <Davidochiengy@gmail.com>
2023-08-23 13:49:40 +0300

    code cleaning

--- main.go
+++ main.go
@@ -70,18 +70,18 @@ func createMovies(w http.ResponseWriter, r *http.Request) {
 func updateMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-Type", "application/json")
 	params := mux.Vars(r)
-	var movie Movie
 
-	_ = json.NewDecoder(r.Body).Decode(&movie)
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(999999999999999999))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
-
-	movie.ID = strconv.Itoa(rand.Intn(999999999999999999))
-	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
