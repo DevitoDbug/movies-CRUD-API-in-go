69862b9c18c844b50c5ab31373a7e0cdff157a45
DevitoDbug <Davidochiengy@gmail.com>
2023-08-23 11:59:55 +0300

    Created routes for the api

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -12,6 +19,31 @@ type Director struct {
 	LastName  string `json:"lastName"`
 }
 
+// Movies -Storing our movies
+var movies []Movie
+
 func main() {
+	port := ":8000"
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "3452134",
+		Title: "The start",
+		Director: &Director{
+			FirstName: "David",
+			LastName:  "Ochieng",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies)
+	r.HandleFunc("/movies/id", getMovie)
+	r.HandleFunc("/movies", createMovies)
+	r.HandleFunc("/movies/id", updateMovies)
+	r.HandleFunc("/movies/id", deleteMovies)
+
+	fmt.Printf("Starting server on port %v\n", port)
+
+	log.Fatal(http.ListenAndServe(port, r))
 
 }
